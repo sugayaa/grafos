c295364f63649c67473bc6eb1f036a342eddb429
sugayaa <vitorsugaya5@gmail.com>
2024-03-29 04:20:40 -0300

    delete function

--- bst.cpp
+++ bst.cpp
@@ -7,7 +7,7 @@ struct node {
 };
 
 struct node* init(int v){
-    struct node* n = new struct node;
+    struct node* n = new struct node();
     n->value = v;
     n->left = NULL;
     n->right = NULL;
@@ -27,6 +27,53 @@ struct node* insert(struct node* node, int v){
     return node;
 }
 
+struct node* deletenode(struct node* root, int v) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (root->value > v) {
+        root->left = deletenode(root->left, v);
+        return root;
+    } else if (root->value < v) {
+        root->right = deletenode(root->right, v);
+        return root;
+    }
+
+    // found deletion node
+    if (root->left == NULL) {
+        struct node* tmp = root->right;
+        delete root;
+        return tmp;
+    } else if (root->right == NULL) {
+        struct node* tmp = root->left;
+        delete root;
+        return tmp;
+    } else {
+        struct node* succParent = root;
+
+        struct node* succ =  root->right;
+        // looking for the smaller value node
+        while (succ->left != NULL) {
+            succParent = succ;
+            succ = succ->left;
+        }
+
+        if (succParent != root) {
+            succParent->left = succ->right;
+        } else {
+            succParent->right = succ->right;
+        }
+
+        // we swap the root value with the sucessor value
+        // and delete the node where the sucessor was
+        root->value = succ->value;
+
+        delete succ;
+        return root;
+    }
+}
+
 void inorder(struct node* node) {
     if (node == NULL)
         return;
@@ -41,7 +88,7 @@ void preorder(struct node* node) {
         return;
 
     cout << node->value << " ";
-    preoder(node->left);
+    preorder(node->left);
     preorder(node->right);
 }
 
